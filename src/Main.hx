51d299ac50f2b433269f0a13547e223445f6ce8d
dallinbeutler <dallinbeutler@gmail.com>
2021-08-05 16:50:04 -0600

    got basic orthocam working

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,4 @@
+import h3d.scene.Light;
 import MouseStuff.MouseHelper;
 import h3d.scene.Mesh;
 import h3d.col.Bounds;
@@ -25,9 +26,18 @@ class Main extends hxd.App {
 
         var root = new scene.ContainerComp(Right,alignment);
         
+        //lighting
+        s3d.lightSystem.ambientLight.set(0.5, 0.5, 0.5);
+        var directionalLight = new h3d.scene.fwd.DirLight(new h3d.Vector(-0.3, -0.2, -1), s3d);
+        directionalLight.enableSpecular = true;
+
+
         // var g3 = new h3d.scene.Graphics(s3d);
         var cube = new Cube();
+        cube.addUVs();
+        cube.addNormals();
         var m = new h3d.scene.Mesh(cube, s3d);
+        
         s3d.addChild(m);
         
         style = new h2d.domkit.Style();
@@ -36,12 +46,14 @@ class Main extends hxd.App {
 
 
         // hxd.Res.props.watch(initCamera);
-        // initCamera();
+        fixOrtho();
 
         // var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
         // tf.text = "Hello Worlds!";
-        new h3d.scene.CameraController(s3d).loadFromCamera();
+        
+        new core.OrthoCameraController(s3d).loadFromCamera();
         var grid = new h3d.prim.Grid(8,8,1,1);
+        grid.addNormals();
         var gridMesh = new Mesh( grid);
         MouseHelper.initInteract(s3d,gridMesh);
         
@@ -49,17 +61,30 @@ class Main extends hxd.App {
         // gridMesh.material.mainPass.wireframe = true;
         s3d.addChild(gridMesh);
     }
-    function initCamera(){
-        var cam = new Camera();
+    
+    var scale = 20*2;//since we sub and add from center
+    function fixOrtho(){
         // var startup:haxe.DynamicAccess<Float> = haxe.Json.parse(hxd.Res.props.entry);
-        
-        cam.orthoBounds = Bounds.fromValues( -5.0,-5.0, 0,5,5,100);
-        s3d.camera = cam;
+        var width = s2d.width / scale;
+        var height = s2d.height / scale;
+        // s3d.camera.orthoBounds = Bounds.fromValues( -5.0,-5.0, 0,5,5,5000);
+        s3d.camera.orthoBounds = Bounds.fromValues( -width,-height, 0,width,height,5000);
     }
+
+    // function initCamera(){
+    //     var cam = new Camera();
+
+
+
+
+        
+    //     s3d.camera = cam;
+    // }
 
     override function onResize() {
 		alignment.minWidth = alignment.maxWidth = s2d.width;
 		alignment.minHeight = alignment.maxHeight = s2d.height;
+        fixOrtho();
 	}
 
     override function update(dt:Float){
